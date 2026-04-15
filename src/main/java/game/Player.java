b38e4abb7aad10916c7ec4d8c10c28e9bed7a2d1
Shaan2050 <teva4812@gmail.com>
2026-04-15 23:05:46 +1000

    Does not JVM arguement. Added player movement. Needs collision.

---
+++ src/main/java/game/Player.java
@@ -0,0 +1,39 @@
+package game;
+
+import bagel.Image;
+import bagel.Input;
+import bagel.Keys;
+
+public class Player{
+    private Image playerImage;
+    private String playerIcon;
+    
+    private int health;
+    final private int speed;
+    private double x;
+    final private double y;
+
+    public Player(String playerIcon, double x, double y, int health, int speed){
+        this.playerIcon = playerIcon;
+        this.x = x;
+        this.y = y;
+        
+        this.health = health;
+        this.speed = speed;
+
+        this.playerImage = new Image(playerIcon);
+    }
+
+    public void movement(Input input){
+        if (input.isDown(Keys.A)) {
+            x -= speed;
+        }
+        if (input.isDown(Keys.D)) {
+            x += speed;
+        }
+    }
+
+    public void draw(){
+        playerImage.draw(x, y);
+    }
+}
